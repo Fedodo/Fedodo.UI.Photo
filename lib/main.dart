b653592ef8c47403a8ad3d7edbcf96f6df9d5ab4
Lukas Nagel <lukas@lna-dev.net>
2023-06-26 07:10:10 +0200

    ✨ Added app creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:fedodo_general/Globals/app_globals.dart';
+import 'package:fedodo_ui_photo/home.dart';
 
 void main() {
-  runApp(const FedodoPhoto());
-}
-
-class FedodoPhoto extends StatelessWidget {
-  const FedodoPhoto({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fedodo Photo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Home(title: 'Fedodo Photo'),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  AppGlobals.createApp(
+    "Fedodo Photo",
+    const Home(),
+  );
 }
