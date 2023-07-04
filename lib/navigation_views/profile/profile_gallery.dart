19cbd7e6b44af0ec0a66f0d8e033aaa1ec06a236
Lukas Nagel <lukas@lna-dev.net>
2023-07-04 06:23:39 +0200

    ✨ Finished profile

--- lib/navigation_views/profile/profile_gallery.dart
+++ lib/navigation_views/profile/profile_gallery.dart
@@ -85,49 +85,53 @@ class _ProfileGalleryState extends State<ProfileGallery> {
         onRefresh: () => Future.sync(
           () => _paginationController.refresh(),
         ),
-        child: PagedGridView<String, Document>(
-          scrollController: null,
-          addSemanticIndexes: false,
-          addAutomaticKeepAlives: false,
-          addRepaintBoundaries: true,
-          clipBehavior: Clip.none,
-          pagingController: _paginationController,
-          builderDelegate: PagedChildBuilderDelegate<Document>(
-            itemBuilder: (context, item, index) => Ink.image(
-              image: CachedNetworkImageProvider(
-                item.url?.asFedodoProxyUri().toString() ?? "",
-              ),
-              child: InkWell(
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   PageRouteBuilder(
-                  //     transitionDuration: const Duration(milliseconds: 300),
-                  //     reverseTransitionDuration:
-                  //         const Duration(milliseconds: 300),
-                  //     pageBuilder: (context, animation, animation2) =>
-                  //         PhotoDetail(
-                  //       url: item.url?.asFedodoProxyUri().toString() ?? "",
-                  //     ),
-                  //     transitionsBuilder:
-                  //         (context, animation, animation2, widget) =>
-                  //             SlideTransition(
-                  //                 position: Tween(
-                  //                   begin: const Offset(1.0, 0.0),
-                  //                   end: const Offset(0.0, 0.0),
-                  //                 ).animate(animation),
-                  //                 child: widget),
-                  //   ),
-                  // );
-                },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: PagedGridView<String, Document>(
+            scrollController: null,
+            addSemanticIndexes: false,
+            addAutomaticKeepAlives: false,
+            addRepaintBoundaries: true,
+            clipBehavior: Clip.none,
+            pagingController: _paginationController,
+            builderDelegate: PagedChildBuilderDelegate<Document>(
+              itemBuilder: (context, item, index) => Ink.image(
+                fit: BoxFit.cover,
+                image: CachedNetworkImageProvider(
+                  item.url?.asFedodoProxyUri().toString() ?? "",
+                ),
+                child: InkWell(
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   PageRouteBuilder(
+                    //     transitionDuration: const Duration(milliseconds: 300),
+                    //     reverseTransitionDuration:
+                    //         const Duration(milliseconds: 300),
+                    //     pageBuilder: (context, animation, animation2) =>
+                    //         PhotoDetail(
+                    //       url: item.url?.asFedodoProxyUri().toString() ?? "",
+                    //     ),
+                    //     transitionsBuilder:
+                    //         (context, animation, animation2, widget) =>
+                    //             SlideTransition(
+                    //                 position: Tween(
+                    //                   begin: const Offset(1.0, 0.0),
+                    //                   end: const Offset(0.0, 0.0),
+                    //                 ).animate(animation),
+                    //                 child: widget),
+                    //   ),
+                    // );
+                  },
+                ),
               ),
             ),
-          ),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 0,
-            childAspectRatio: 2,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              childAspectRatio: 1,
+            ),
           ),
         ),
       ),
